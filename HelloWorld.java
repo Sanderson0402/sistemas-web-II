f15d4ce56510c52dcbccc4124fb11616ed120996
Sanderson Milagres <58516606+Sanderson0402@users.noreply.github.com>
2023-09-12 18:56:44 -0300

    simples Hello World!

---
+++ HelloWorld.java
@@ -0,0 +1,28 @@
+package exercicios;
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+    public HelloWorld() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/plain");
+		response.getWriter().print("<h1>Hello World!<h1>");
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
